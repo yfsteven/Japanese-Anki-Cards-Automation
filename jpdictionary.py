c51670946b8e11069db5d33c8541876bc394fbd1
yfsteven <152656884+yfsteven@users.noreply.github.com>
2024-04-26 09:31:52 -0400

    Improved functionality

--- jpdictionary.py
+++ jpdictionary.py
@@ -4,7 +4,6 @@ import requests, bs4, sys, os, csv
 from dotenv import load_dotenv
 from elevenlabs import Voice, VoiceSettings, play, save
 from elevenlabs.client import ElevenLabs
-import os
 
 os.makedirs('anki_csv', exist_ok=True)
 
@@ -29,21 +28,22 @@ if len(sys.argv) > 1:
             for element in jp_text:
                 full_sentence += element.text
 
-            with open(os.path.join('anki_csv', os.path.basename('words.csv')), 'a', newline='') as textfile: #Adds in at the last row
-                csvwriter = csv.writer(textfile)
-                csvwriter.writerow([f'{word}', f'{full_sentence}'])
-            print(full_sentence)
             audio = client.generate(
                 text = full_sentence,
                 model="eleven_multilingual_v2",
                 voice=Voice(
-                    voice_id="j210dv0vWm7fCknyQpbA",
+                    voice_id="j210dv0vWm7fCknyQpbA", #Japanese Voice ID
                     settings=VoiceSettings(stability=0.50, similarity_boost=0.65, style=0, use_speaker_boost=True),
                 )
             )
 
-            play(audio) #Generate audio for defintion that can be added. Word -> Definition -> Audio
+            save(audio, f"/home/vboxuser/.local/share/Anki2/ユーザー 1/collection.media/{word}.wav") #May be different from yours, but this saves audio to Anki's media collection folder. Must do this or else not able to import media files
 
+
+            with open(os.path.join('anki_csv', os.path.basename('words.csv')), 'a', newline='') as textfile: #Adds in at the last row of CSV
+                csvwriter = csv.writer(textfile)
+                csvwriter.writerow([f'{word}', f'{full_sentence}', f'[sound:{word}.wav]'])
+            print(full_sentence)
 
         print('Done')
     except Exception as err:
