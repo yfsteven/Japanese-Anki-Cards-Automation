b0fc5adb0fbb3f948c088a21f93ca27ff79e52c7
yfsteven <152656884+yfsteven@users.noreply.github.com>
2024-05-19 14:18:10 -0400

    Added sync functionality thanks to anki-csv-importer

--- jpdictionary.py
+++ jpdictionary.py
@@ -1,16 +1,21 @@
 #! python3
-# jpdictionary.py - web scrabe goojp of definition and convert the data into a single csv file that can be easily imported to Anki
-import requests, bs4, sys, os, csv
+# jpdictionary.py - web scrap goojp dictionary and convert the data into a single csv file that can be easily imported to Anki
+import requests
+import bs4
+import sys
+import os
+import csv
+from ankicsvimporter import send_to_anki_connect, invoke_ac
 from dotenv import load_dotenv
-from elevenlabs import Voice, VoiceSettings, play, save
+from elevenlabs import Voice, VoiceSettings, save
 from elevenlabs.client import ElevenLabs
 
-os.makedirs('anki_csv', exist_ok=True)
+os.makedirs('anki_csv', exist_ok=True) # creates a folder to hold csv file
 
 load_dotenv()
 
 client = ElevenLabs(
-  api_key=os.getenv("ELEVEN_LAB_API_KEY"),
+  api_key=os.getenv("ELEVEN_LAB_API_KEY"), # insert api key
 )
 
 
@@ -37,17 +42,24 @@ if len(sys.argv) > 1:
                 )
             )
 
-            save(audio, f"/home/vboxuser/.local/share/Anki2/ユーザー 1/collection.media/{word}.wav") #May be different from yours, but this saves audio to Anki's media collection folder. Must do this or else not able to import media files
+            save(audio, f"/home/vboxuser/.local/share/Anki2/ユーザー 1/collection.media/{word}.wav") #May be different from yours, but this saves audio to Anki's media collection folder. Must do this or else not able to get media files
 
 
             with open(os.path.join('anki_csv', os.path.basename('words.csv')), 'a', newline='') as textfile: #Adds in at the last row of CSV
-                file_is_empty = os.stat(os.path.join('anki_csv', os.path.basename('words.csv'))).st_size == 0
-                csvwriter = csv.DictWriter(textfile, delimiter=',', fieldnames=['表面', '裏面', 'Audio'])
+                file_is_empty = os.stat('./anki_csv/words.csv').st_size == 0
+                csvwriter = csv.DictWriter(textfile, delimiter=',', fieldnames=['表面', '裏面', 'Audio']) # you must include fieldnames and must be correct for anki-csv-importer program to work
                 if file_is_empty:
-                    csvwriter.writeheader()
+                    csvwriter.writeheader() # writes fieldname if empty however limitation
                 csvwriter.writerow({'表面': f'{word}', '裏面': f'{full_sentence}', 'Audio': f'[sound:{word}.wav]'})
 
             print(full_sentence)
+
+            send_to_anki_connect( #import csv file and sync anki. Must install ankiconnect addon. Please read anki-csv-importer documentation
+                './anki_csv/words.csv', #path to csv file
+                'デフォルト', # deckname
+                '基本') # note type
+            print('[+] Syncing')
+            invoke_ac("sync")
 
         print('Done')
     except Exception as err:
