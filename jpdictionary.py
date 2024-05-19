ae5acc847d333c028f71fc4a8c5ca0bd7886c6ea
yfsteven <152656884+yfsteven@users.noreply.github.com>
2024-05-19 01:06:39 -0400

    Changed csv file to have a better format

--- jpdictionary.py
+++ jpdictionary.py
@@ -10,7 +10,7 @@ os.makedirs('anki_csv', exist_ok=True)
 load_dotenv()
 
 client = ElevenLabs(
-  api_key=os.getenv("API_KEY"),
+  api_key=os.getenv("ELEVEN_LAB_API_KEY"),
 )
 
 
@@ -41,8 +41,12 @@ if len(sys.argv) > 1:
 
 
             with open(os.path.join('anki_csv', os.path.basename('words.csv')), 'a', newline='') as textfile: #Adds in at the last row of CSV
-                csvwriter = csv.writer(textfile)
-                csvwriter.writerow([f'{word}', f'{full_sentence}', f'[sound:{word}.wav]'])
+                file_is_empty = os.stat(os.path.join('anki_csv', os.path.basename('words.csv'))).st_size == 0
+                csvwriter = csv.DictWriter(textfile, delimiter=',', fieldnames=['表面', '裏面', 'Audio'])
+                if file_is_empty:
+                    csvwriter.writeheader()
+                csvwriter.writerow({'表面': f'{word}', '裏面': f'{full_sentence}', 'Audio': f'[sound:{word}.wav]'})
+
             print(full_sentence)
 
         print('Done')
